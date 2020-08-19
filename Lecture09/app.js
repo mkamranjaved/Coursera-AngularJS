bfd11226118a3b77a51388a4866bea5de4aeaa48
mkamranjaved <mkamranjaved@hotmail.com>
2020-08-19 22:01:28 +0500

    Lecture7 Files Final

--- Lecture09/app.js
+++ Lecture09/app.js
@@ -2,8 +2,18 @@
   'use strict';
 
   angular.module('DIApp', [])
-  .controller('DIController', function($scope){
+  .controller('DIController', DIController);
+
+  function DIController ($scope, $filter, $injector) {
   $scope.name = "Kamran";
 
-  });
+  $scope.upper = function(){
+    var upCase = $filter('uppercase');
+    $scope.name = upCase($scope.name);
+  };
+
+  console.log(DIController.toString());
+  console.log($injector.annotate(DIController));
+  }
+
 })();
