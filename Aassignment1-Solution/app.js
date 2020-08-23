d97e6c9b468447720f364f59970ebd18c9660303
mkamranjaved <mkamranjaved@hotmail.com>
2020-08-23 18:27:43 +0500

    Assignment1 Solution Submitted

---
+++ Aassignment1-Solution/app.js
@@ -0,0 +1,37 @@
+(function(){
+  'use strict';
+
+  angular.module('lunchCheck', [])
+  .controller('lunchCheckController', lunchCheckController);
+
+    lunchCheckController.inject = ['$scope'];
+
+    function lunchCheckController($scope){
+    $scope.inputDishes = "";
+    $scope.outputMsg = "";
+
+    $scope.checkDishes = function(){
+      $scope.outputMsg = countDishes($scope.inputDishes);
+    };
+
+    function countDishes(inputDishes){
+
+      var arrayOfDishes = inputDishes.split(',');
+      var totalDishes = 0; //arrayOfDishes.length;
+
+      for (var i = 0; i < arrayOfDishes.length; i++){
+        if (arrayOfDishes[i].trim().length != 0)
+           totalDishes ++;
+      }
+      if (totalDishes === 0)
+        return "Empty Dishes are not allowed. Please enter valid Dishes.";
+      else {
+      if (totalDishes <= 3)
+        return "Enjoy the Lunch! " +totalDishes;// + totalLength;
+      else if (totalDishes > 3)
+        return "Sorry, too much dishes. Dishes count should not exceed 3. " +totalDishes;
+      }
+    };
+
+  }
+})();
