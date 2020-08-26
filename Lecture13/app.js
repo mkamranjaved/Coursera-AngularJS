f6f91d03ba5e9478c65bfd375962aa0e76eeba8c
mkamranjaved <mkamranjaved@hotmail.com>
2020-08-26 20:15:32 +0500

    Chapter 13

--- Lecture13/app.js
+++ Lecture13/app.js
@@ -3,7 +3,8 @@
 
   angular.module('MsgApp', [])
   .controller('MsgController', MsgController)
-  .filter('loves', LovesFilter);
+  .filter('loves', LovesFilter)
+  .filter('truth', TruthFilter);
 
   MsgController.$inject = ['$scope', 'lovesFilter'];
   function MsgController($scope, lovesFilter){
@@ -31,6 +32,14 @@
     return function (input) {
       input = input || "";
       input = input.replace("likes", "loves")
+      return input;
+    }
+  };
+
+  function TruthFilter(){
+    return function (input, target, replace) {
+      input = input || "";
+      input = input.replace(target, replace);
       return input;
     }
   };
